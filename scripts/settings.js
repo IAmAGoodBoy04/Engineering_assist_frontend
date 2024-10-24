a582bb77e8113e85c8d6d9e50b72fe23d7328950
Shubhan <shubhan.singh@gmail.com>
2024-10-24 10:11:09 +0530

    10:11AM

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,3 +1,13 @@
+const URL = "http://localhost:8080";
+
+const userId=sessionStorage.getItem('userId');
+const token=sessionStorage.getItem('token');
+const email=decodeURIComponent(sessionStorage.getItem('email'));
+const username = decodeURIComponent(sessionStorage.getItem('username'));
+const flask_url = sessionStorage.getItem('fetchedUrl');
+const textbook=sessionStorage.getItem('topic');
+
+
 document.getElementById('temperature').oninput = function() {
     document.getElementById('temperatureValue').innerHTML = parseFloat(this.value).toFixed(2);
 }
@@ -8,5 +18,29 @@ const backButton = document.getElementById('back-button');
 if (backButton) {
     backButton.addEventListener('click', () => {
         window.location.href = './homepage.html';
+    });
+}
+
+const adminButton = document.getElementById('admin_button');
+if (adminButton) {
+    adminButton.addEventListener('click', async () => {
+        try {
+            const response = await fetch(`${URL}/auth/isadmin?userId=${userId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (response.status === 200) {
+                window.location.href = './admin.html';
+            } else {
+                alert('You are not an admin.');
+            }
+        } catch (error) {
+            console.error('Error checking admin status:', error);
+            alert('An error occurred. Please try again.');
+        }
     });
 }
